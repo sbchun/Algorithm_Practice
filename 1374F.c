288fd7dc78c86788bc949faa5b7f1fbfc35d7fe9
Chun Seung Bum <sbchun09@gmail.com>
2020-07-04 11:21:28 +0900

    a
    a

---
+++ 1374F.c
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+int arr[109][509];
+
+int main(){
+
+
+    int t;
+    int arr_size[];
+    printf("test case : ");
+    scanf("%d", t);
+    
+
+
+    for(int i = 0 ; i < t ; i++){
+
+
+        printf("arr case : ");
+    
+    }
+
+    return 0;
+}
